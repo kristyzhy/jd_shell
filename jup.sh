d837fa8ec17868410376369cb9232fa9f50a7dcc
gualy187 <4978089+gualy187@user.noreply.gitee.com>
2021-05-22 17:13:57 +0800

    更改 url

--- jup.sh
+++ jup.sh
@@ -3,8 +3,8 @@
 ## 文件路径、脚本网址
 dir_shell=$(dirname $(readlink -f "$0"))
 dir_root=$dir_shell
-url_shell=${JD_SHELL_URL:-git@jd_shell_gitee:evine/jd_shell.git}
-url_scripts=${JD_SCRIPTS_URL:-git@jd_scripts_gitee:lxk0301/jd_scripts.git}
+url_shell=${JD_SHELL_URL:-https://gitee.com/gualy187/jd_shell.git}
+url_scripts=${JD_SCRIPTS_URL:-https://gitee.com/gualy187/jd_scripts.git}
 send_mark=$dir_shell/send_mark
 
 ## 导入通用变量与函数
